73df683c97211a3309b30616d3eae1ca3532187b
MohitDhungana <dhunganamohit@gmail.com>
2022-03-20 23:27:00 +0545

    changed filename to remove package import conflict

--- matplotlib-learn.py
+++ matplotlib-learn.py
@@ -18,16 +18,17 @@ plt.xlim(-1, 12)
 plt.ylim(-1.5, 1.5)
 
 # save image to local file
-# fig.savefig("my_figure.png")
+# * fig.savefig("my_figure.png")
 plt.show()
 
 # %%
 fig = plt.figure()
+ax = plt.axes()
 x = np.linspace(0, 10, 11)
 
 
 plt.plot(x, x + 1, "-", color="red")
-plt.plot(x, x ** 2, "--")
+plt.plot(x, x**2, "--")
 
 
 fig.savefig("my_figure.png")
@@ -41,3 +42,5 @@ plt.plot(x, np.sin(x))
 plt.subplot(2, 1, 1)
 plt.plot(x, np.cos(x))
 # %%
+print("fig", fig)
+print("ax", ax)
